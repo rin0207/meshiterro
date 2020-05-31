c52e36244f9c0d4203a07e7b57f6e22155cf32a0
rin0207 <rintaku.7@gmail.com>
2020-06-01 06:03:53 +0900

    [start]5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'c746381b8dabc0ad19761f937c6e90ab30f32feac7a2af0bca36e96733bf1776c6efa64038eacb4b29dbaf944fe4cca89540cc675a041c389b239e000c8f416c'
